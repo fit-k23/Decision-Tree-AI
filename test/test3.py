1278bd549a0c9bcb83b97623c91001c872ef3280
NTT1906 <nttispay@gmail.com>
2025-04-24 17:40:29 +0700

    update

--- test/test3.py
+++ test/test3.py
@@ -11,3 +11,12 @@ datasets[datasets_items[0]] = pd.read_csv(palmerpenguins_db_url)
 datasets[datasets_items[1]] = pd.read_csv(heart_disease_db_url, names=heart_disease_columns, na_values='?')
 
 print(datasets)
+
+heart_disease_db = datasets[datasets_items[1]]
+# X = heart_disease_db.drop(['num'], axis=1)
+# y = heart_disease_db['num']
+
+from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.5)
+X_train, X_test = train_test_split(heart_disease_db,test_size=0.5)
+print(X_train, '\n', X_test)
